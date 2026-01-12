a2c454c2de1d014086ca459eea0576c8ab7352f1
Ilnitsky <ilnitsky-dev@yandex.ru>
2026-01-13 04:41:48 +0700

    Changed eslint config

--- Ilnitsky.Polls/front_app/eslint.config.js
+++ Ilnitsky.Polls/front_app/eslint.config.js
@@ -1,3 +1,16 @@
-import vuetify from 'eslint-config-vuetify'
+import vuetify from 'eslint-config-vuetify';
 
-export default vuetify()
+export default vuetify(
+  {
+    rules: {
+      // Указываем стилистическому плагину всегда требовать точки с запятой
+      '@stylistic/semi': ['error', 'always'],
+
+      // На всякий случай явно настраиваем и базовое правило
+      'semi': ['error', 'always'],
+
+      // Отключаем правило, которое может ругаться на "лишние" точки
+      '@stylistic/no-extra-semi': 'error',
+    },
+  },
+);
