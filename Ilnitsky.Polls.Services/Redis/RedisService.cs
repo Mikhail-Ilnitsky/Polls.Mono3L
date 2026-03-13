7a37906a5b8da5f8f0ca5082c4ac4ab3afbfd86f
Ilnitsky <ilnitsky-dev@yandex.ru>
2026-03-14 03:29:09 +0700

    [back] Added logger to RedisService

--- Ilnitsky.Polls.Services/Redis/RedisService.cs
+++ Ilnitsky.Polls.Services/Redis/RedisService.cs
@@ -2,47 +2,81 @@ using System.Text.Json;
 
 using Ilnitsky.Polls.Services.OptionsProviders;
 
+using Microsoft.Extensions.Logging;
+
 using StackExchange.Redis;
 
 namespace Ilnitsky.Polls.Services.Redis;
 
 public class RedisService(
     ICacheOptionsProvider cacheOptions,
-    IConnectionMultiplexer connectionMultiplexer)
+    IConnectionMultiplexer connectionMultiplexer,
+    ILogger<RedisService> logger)
         : IRedisService
 {
-    private readonly IDatabase _db = connectionMultiplexer.GetDatabase();
+    private readonly IDatabase _db = connectionMultiplexer?.GetDatabase() ?? throw new ArgumentNullException(nameof(connectionMultiplexer));
     private readonly ICacheOptionsProvider _cacheOptions = cacheOptions ?? throw new ArgumentNullException(nameof(cacheOptions));
+    private readonly ILogger<RedisService> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
     public async Task SetAsync<T>(string key, T? value, TimeSpan? expiration = null)
     {
-        expiration ??= _cacheOptions.DefaultExpiration;
+        try
+        {
+            expiration ??= _cacheOptions.DefaultExpiration;
 
-        var valueString = value is null
-            ? "ABSENT"
-            : JsonSerializer.Serialize(value);
+            var valueString = value is null
+                ? "ABSENT"
+                : JsonSerializer.Serialize(value);
 
-        await _db.StringSetAsync(key, valueString, expiration);
+            var isSetted = await _db.StringSetAsync(key, valueString, expiration);
+
+            _logger.LogDebug("Redis-cache SET: {IsSetted} for Key={Key}, Expiration={Expiration}", isSetted, key, expiration);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error setting Redis-cache for key={Key}", key);
+        }
     }
 
     public async Task<(bool HasValue, T? Value)> GetAsync<T>(string key)
     {
-        var redisValue = await _db.StringGetAsync(key);
-
-        if (redisValue.IsNull)
+        try
         {
+            var redisValue = await _db.StringGetAsync(key);
+
+            _logger.LogDebug(
+                "Redis-сache GET: {Result} for Кey={Key}",
+                redisValue.IsNull ? "MISS" : "HIT",
+                key);
+
+            if (redisValue.IsNull)
+            {
+                return (false, default);
+            }
+            if (redisValue == "ABSENT")
+            {
+                return (true, default);
+            }
+
+            return (true, JsonSerializer.Deserialize<T>(redisValue!));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error getting from Redis-cache for Key={Key}", key);
             return (false, default);
         }
-        if (redisValue == "ABSENT")
-        {
-            return (true, default);
-        }
-
-        return (true, JsonSerializer.Deserialize<T>(redisValue!));
     }
 
     public async Task RemoveAsync(string key)
     {
-        await _db.KeyDeleteAsync(key);
+        try
+        {
+            var isDeleted = await _db.KeyDeleteAsync(key);
+            _logger.LogDebug("Redis-сache REMOVE: {IsDeleted} for Key={Key}", isDeleted, key);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error removing from Redis-cache for Key={Key}", key);
+        }
     }
 }
