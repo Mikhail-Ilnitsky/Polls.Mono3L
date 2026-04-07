40b5eeaea748ce4d979e00d263d71eba79ada25b
Ilnitsky <ilnitsky-dev@yandex.ru>
2026-04-07 17:00:01 +0700

    [tests] Aded unit tests for GetPollLinksHandler

--- Ilnitsky.Polls.Tests.XUnit/Unit/Handlers/GetPollByIdHandlerTests.cs
+++ Ilnitsky.Polls.Tests.XUnit/Unit/Handlers/GetPollByIdHandlerTests.cs
@@ -6,7 +6,6 @@ using Ilnitsky.Polls.BusinessLogic.Handlers.Polls;
 using Ilnitsky.Polls.Contracts.Dtos;
 using Ilnitsky.Polls.Contracts.Dtos.Polls;
 using Ilnitsky.Polls.DataAccess;
-using Ilnitsky.Polls.DataAccess.Entities.Polls;
 using Ilnitsky.Polls.DbInitialization;
 using Ilnitsky.Polls.Services.DualCache;
 using Ilnitsky.Polls.Services.OptionsProviders;
@@ -45,7 +44,7 @@ public class GetPollByIdHandlerTests : IDisposable
         _memoryOptions = new(memoryCacheOptions);
         _redisOptions = new(redisCacheOptions);
 
-        // Создаем уникальную БД в памяти для каждого теста
+        // Создаем уникальное имя БД для каждого запуска теста
         var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
             .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
             .Options;
@@ -53,47 +52,12 @@ public class GetPollByIdHandlerTests : IDisposable
         _dbContext = new ApplicationDbContext(dbOptions);
     }
 
-    private static (Poll PollEntity, Guid PollId, string PollKey) GetPoll()
-    {
-        var pollId = DbInitializer.CreateGuidV7();
-        var pollKey = $"api_poll_{pollId}";
-        var pollEntity = new Poll
-        {
-            Id = pollId,
-            DateTime = DateTime.UtcNow,
-            Name = "Марки китайских автомобилей",
-            Html = "<img class=\"mb-1 w-100\" src=\"https://infotables.ru/images/avto/logo_auto/logo_china_auto.png\">",
-            IsActive = true,
-            Questions = [
-                new Question
-                {
-                    Id = pollId,
-                    Text = "Какие марки китайских автомобилей вы знаете?",
-                    AllowCustomAnswer = false,
-                    AllowMultipleChoice = true,
-                    Number = 1,
-                    TargetAnswer = null,
-                    MatchNextNumber = null,
-                    DefaultNextNumber = null,
-                    Answers = DbInitializer.CreateAnswers([
-                        "Brilliance", "BYD", "Changan", "Chery", "Dongfeng",
-                        "FAW", "Foton", "GAC", "Geely", "Great Wall",
-                        "Hafei", "Haima", "Haval", "Hawtai", "JAC",
-                        "Lifan", "Zotye",
-                    ]),
-                },
-            ],
-        };
-
-        return (pollEntity, pollId, pollKey);
-    }
-
     [Fact]
     public async Task HandleAsync_ReturnsSuccess_WhenPollFoundInCache()
     {
         // Arrange
 
-        var (pollEntity, pollId, pollCacheKey) = GetPoll();
+        var (pollEntity, pollId, pollCacheKey) = TestDbHelper.CreatePoll();
         var pollDto = pollEntity.ToDto();
 
         // Мокаем в кэше нужное значение для заданного pollCacheKey, и отсутствие значений для других ключей
@@ -122,7 +86,7 @@ public class GetPollByIdHandlerTests : IDisposable
     public async Task HandleAsync_ReturnsSuccess_WhenPollExistsInDatabaseOnly()
     {
         // Arrange
-        var (pollEntity, pollId, pollCacheKey) = GetPoll();
+        var (pollEntity, pollId, pollCacheKey) = TestDbHelper.CreatePoll();
 
         // Добавляем в БД
         _dbContext.Polls.Add(pollEntity);
@@ -185,7 +149,7 @@ public class GetPollByIdHandlerTests : IDisposable
     public async Task HandleAsync_ReturnsNotFound_WhenPollAbsenceIsCached()
     {
         // Arrange
-        var (pollEntity, pollId, _) = GetPoll();
+        var (pollEntity, pollId, _) = TestDbHelper.CreatePoll();
 
         // Добавляем в БД чтобы убедиться, что не будет обращения к БД
         _dbContext.Polls.Add(pollEntity);
