755a195696696e8cd34efd7983aee2695a3c3da0
Ilnitsky <ilnitsky-dev@yandex.ru>
2026-04-28 15:46:42 +0700

    [tests] Fixed fluent tests for AnswersController

--- Ilnitsky.Polls.Tests.XUnit.Fluent/Controllers/AnswersControllerTests.cs
+++ Ilnitsky.Polls.Tests.XUnit.Fluent/Controllers/AnswersControllerTests.cs
@@ -50,9 +50,9 @@ public class AnswersControllerTests
     {
         _httpContext.Items
             .Should().ContainKey("ErrorDetails")
-                .WhoseValue.As<string>()
-                .Should().StartWith(expectedStart)
-                .And.Contain(expectedValue);
+                .WhoseValue.Should().BeOfType<string>()
+                    .Which.Should().StartWith(expectedStart)
+                    .And.Contain(expectedValue);
     }
 
     [Fact]
