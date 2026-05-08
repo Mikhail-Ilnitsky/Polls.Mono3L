f7e4e692c5a5d7f53195fba3dfd031b6b8a1e724
Ilnitsky <ilnitsky-dev@yandex.ru>
2026-05-08 20:13:14 +0700

    [tests] Added AnswersApiTests

---
+++ Ilnitsky.Polls.Tests.Integration.NUnit/FactoryTestExtensions.cs
@@ -0,0 +1,28 @@
+using System.Net;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using Microsoft.AspNetCore.Mvc.Testing.Handlers;
+
+namespace Ilnitsky.Polls.Tests.Integration.NUnit;
+
+public static class FactoryTestExtensions
+{
+    public static HttpClient GetHttpClientWithCookies(this WebApplicationFactory<Program> factory)
+    {
+        return factory.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            // Автоматически сохранять куки между запросами одного клиента
+            HandleCookies = true,
+            // (Опционально) Запретить автоматические редиректы, чтобы проверять 301/302 ответы
+            AllowAutoRedirect = false,
+        });
+    }
+
+    public static HttpClient GetHttpClientWithCookieContainer(
+        this WebApplicationFactory<Program> factory,
+        CookieContainer cookieContainer)
+    {
+        return factory.CreateDefaultClient(new CookieContainerHandler(cookieContainer));
+    }
+}
