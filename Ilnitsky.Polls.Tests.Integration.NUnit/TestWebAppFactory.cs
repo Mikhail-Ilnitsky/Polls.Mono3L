1d9d88da4476683cfec7aeeca5503fbf94078da0
Ilnitsky <ilnitsky-dev@yandex.ru>
2026-05-22 15:30:29 +0700

    [tests] Removed UseEnvironment("Development")

--- Ilnitsky.Polls.Tests.Integration.NUnit/TestWebAppFactory.cs
+++ Ilnitsky.Polls.Tests.Integration.NUnit/TestWebAppFactory.cs
@@ -11,8 +11,6 @@ public class TestWebAppFactory(string dbConnectionString, string redisConnection
 
     protected override IHost CreateHost(IHostBuilder builder)
     {
-        builder.UseEnvironment("Development");
-
         // Этот метод вызывается в самом начале построения Host, 
         // перекрывая параметры до вызова AutoDetect в Program.cs
         builder.ConfigureAppConfiguration((context, configBuilder) =>
